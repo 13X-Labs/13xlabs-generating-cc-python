821930334eb228221966567f339378859bf852fd
Hồ Ngọc Hải <hongochai10@icloud.com>
2023-04-23 10:11:21 +0700

    update

--- README.md
+++ README.md
@@ -1,7 +1,59 @@
 # 13xlabs Generating CreditCard - Python
 
-13xlabs-generating-cc-python is a tool developed by 13xlabs that allows the creation of random or custom credit card numbers in Python.
+## Credit Card Generator
 
-This tool can be useful for application developers or security experts who want to test the security of their payment system by generating fake credit card numbers for testing purposes.
+This Python script generates a list of 100 credit card numbers based on user input for the prefix, CCV, and expiration date. If the user does not provide a CCV or expiration date, the script generates random values for these fields. The generated credit card numbers are Luhn-compliant and have a total length of 16 digits.
 
-13xlabs-generating-cc-python is an open-source tool that can be customized and used for free for testing and development purposes. However, we encourage the responsible and reasonable use of this tool and compliance with privacy and payment data protection regulations.
+## Requirements
+
+- Python 3.x
+
+## Usage
+
+Run the script in a Python 3 environment:
+
+```bash
+python credit_card_generator.py
+```
+
+The script will prompt you to enter the following information:
+- Desired prefix for the credit card number (maximum 15 digits)
+- Desired CCV (3 digits) or leave it blank to generate randomly
+- Desired expiration date (in MM/YY format) or leave it blank to generate randomly
+
+The script will generate a list of 100 credit card numbers using the provided prefix, CCV, and expiration date (or randomly generated values if the user left them blank). The generated numbers will be written to a file named credit_cards_generation.txt, with each credit card number, expiration date, and CCV separated by a pipe (|) character.
+
+Example output:
+
+```python
+5568839172436580|12/27|123
+5568838247193010|11/24|321
+...
+```
+
+
+## Disclaimer
+This script is for educational purposes only. Generating and using fake credit card numbers is illegal and unethical. Please do not use this script for any illegal activities. The author is not responsible for any consequences resulting from the misuse of this script.
+
+## License
+MIT License
+
+Copyright (c) 2023 Velik Ho
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- cc.py
+++ cc.py
@@ -1,15 +1,18 @@
 import random
 import datetime
+import re
 
-def generate_credit_card_number():
-    # Generate the first 10 digits randomly
-    prefix = "552426"
-    account_number = str(random.randint(0, 999999999)).zfill(9)
+def generate_credit_card_number(prefix, ccv=None, expiration_date=None):
+    # Ensure the prefix is a string
+    prefix = str(prefix)
+    
+    # Generate the remaining digits randomly
+    account_number = str(random.randint(0, 10**(16-len(prefix)) - 1)).zfill(16 - len(prefix))
     card_number = prefix + account_number
 
     # Calculate the Luhn checksum
     checksum = 0
-    for i, digit in enumerate(card_number):
+    for i, digit in enumerate(card_number[:-1]):
         if i % 2 == 0:
             # Double every other digit, starting from the right
             doubled_digit = int(digit) * 2
@@ -23,28 +26,50 @@ def generate_credit_card_number():
 
     # Add the checksum digit to the end of the card number
     if checksum % 10 == 0:
-        card_number += "0"
+        card_number = card_number[:-1] + "0"
     else:
-        card_number += str(10 - (checksum % 10))
+        card_number = card_number[:-1] + str(10 - (checksum % 10))
 
-    # Generate the CCV code (3 digits)
-    ccv = str(random.randint(0, 999)).zfill(3)
+    # If the user doesn't provide a CCV, generate it randomly (3 digits)
+    if ccv is None:
+        ccv = str(random.randint(0, 999)).zfill(3)
 
-    # Generate the expiration date (MM/YY format)
-    now = datetime.datetime.now()
-    expiration_year = now.year + random.randint(1, 6)
-    expiration_month = random.randint(1, 12)
-    if expiration_month < 10:
-        expiration_month = "0" + str(expiration_month)
-    else:
-        expiration_month = str(expiration_month)
-    expiration_date = expiration_month + "/" + str(expiration_year)[2:]
+    # If the user doesn't provide an expiration date, generate it randomly (MM/YY format)
+    if expiration_date is None:
+        now = datetime.datetime.now()
+        expiration_year = now.year + random.randint(1, 6)
+        expiration_month = random.randint(1, 12)
+        if expiration_month < 10:
+            expiration_month = "0" + str(expiration_month)
+        else:
+            expiration_month = str(expiration_month)
+        expiration_date = expiration_month + "/" + str(expiration_year)[2:]
 
     return (card_number, ccv, expiration_date)
 
+# Get user input for the prefix, CCV, and expiration date
+prefix_input = input("Enter the desired prefix for the credit card number (max 15 digits): ")
+
+while True:
+    ccv_input = input("Enter the desired CCV (3 digits) or leave blank to generate randomly: ")
+    if ccv_input.strip() == "" or (len(ccv_input) == 3 and ccv_input.isdigit()):
+        break
+    else:
+        print("Error: Invalid CCV. Please enter a 3-digit number or leave it blank.")
+
+while True:
+    expiration_date_input = input("Enter the desired expiration date (MM/YY) or leave blank to generate randomly: ")
+    if expiration_date_input.strip() == "" or re.match(r"^(0[1-9]|1[0-2])/\d{2}$", expiration_date_input):
+        break
+    else:
+        print("Error: Invalid expiration date. Please enter in the format MM/YY or leave it blank.")
+
+# Set CCV and expiration date to None if they are not provided
+ccv_input = None if ccv_input.strip() == "" else ccv_input
+expiration_date_input = None if expiration_date_input.strip() == "" else expiration_date_input
 
 with open("credit_cards_generation.txt", "w") as f:
     for i in range(100):
         # Generate a new credit card number
-        card_number, ccv, expiration_date = generate_credit_card_number()
+        card_number, ccv, expiration_date = generate_credit_card_number(prefix_input, ccv_input, expiration_date_input)
         f.write(card_number + "|" + expiration_date + "|" + ccv + "\n")
